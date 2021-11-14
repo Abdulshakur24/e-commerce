4d1d3d43b89279da51e3d25feeefba4c93863ef6
abdulshakur <abdulshakur486@gmail.com>
2021-11-14 20:41:49 +0300

    fixed configs

--- database/db.js
+++ database/db.js
@@ -1,21 +1,22 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-// const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_PORT}:${process.env.PG_HOST}/${process.env.PG_DATABASE}`;
+const devConfig = new Pool({
+  user: "postgres",
+  password: "postgres",
+  port: 5432,
+  host: "localhost",
+  database: "audiophile",
+});
 
-const pool = new Pool({
+const productionConfig = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
 
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "postgres",
-//   port: 5432,
-//   host: "localhost",
-//   database: "audiophile",
-// });
+const pool =
+  process.env.NODE_ENV === "production" ? productionConfig : devConfig;
 
 module.exports = pool;
