3a1cb99a783ed4dd6288dda707e30c10d4f2c8c0
abdulshakur <abdulshakur486@gmail.com>
2022-05-30 15:00:45 +0300

    deleted.

--- routers/token.js
+++
@@ -1,8 +0,0 @@
-const tokenRoute = require("express").Router();
-
-const auth = require("../authentication/auth");
-const { token } = require("../controllers/controllers");
-
-tokenRoute.post("/", auth, token);
-
-module.exports = tokenRoute;
