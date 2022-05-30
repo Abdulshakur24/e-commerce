9c2c0a905dbee2770bdc4803e5158403c0619169
abdulshakur <abdulshakur486@gmail.com>
2022-05-30 14:53:07 +0300

    improved features.

--- client/src/components/home/sub-components/Banner.js
+++ client/src/components/home/sub-components/Banner.js
@@ -5,25 +5,27 @@ function Banner() {
   const history = useHistory();
   return (
     <section className="home-banner">
-      <div className="banner-titles">
-        <div className="banner-title a">
-          <p>NEW PRODUCT</p>
-        </div>
+      <div className="home-container">
+        <div className="banner-titles">
+          <div className="banner-title a">
+            <p>NEW PRODUCT</p>
+          </div>
 
-        <div className="banner-title b">
-          <h4>XX99 MARK II</h4>
-          <h4>HEADPHONE</h4>
-        </div>
-        <div className="banner-title c">
-          <p>
-            Experience natural, lifelike audio and exceptional build quality
-            made for the passionate music enthusiast.
-          </p>
-        </div>
-        <div className="banner-title d">
-          <button onClick={() => history.push("/categories/headphones")}>
-            SEE PRODUCT
-          </button>
+          <div className="banner-title b">
+            <h4>XX99 MARK II</h4>
+            <h4>HEADPHONE</h4>
+          </div>
+          <div className="banner-title c">
+            <p>
+              Experience natural, lifelike audio and exceptional build quality
+              made for the passionate music enthusiast.
+            </p>
+          </div>
+          <div className="banner-title d">
+            <button onClick={() => history.push("/categories/headphones")}>
+              SEE PRODUCT
+            </button>
+          </div>
         </div>
       </div>
     </section>
