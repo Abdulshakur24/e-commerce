dafda30ecc22c9f9b68b7dbf3809c205fa6bcfc0
abdulshakur <abdulshakur486@gmail.com>
2022-07-14 23:01:51 +0300

    initial commit.

---
+++ client/src/components/ProtectedRoutes.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useAuth } from "src/hooks/useAuth";
+
+function ProtectedRoutes() {
+  const navigator = useNavigate();
+  const { user } = useAuth((state) => state);
+
+  useEffect(() => {
+    if (!user) return navigator("/register", { replace: true });
+  }, [navigator, user]);
+
+  return <Outlet />;
+}
+
+export default ProtectedRoutes;
