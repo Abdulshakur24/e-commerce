e44737df716645382e0089866e939246b1a11606
abdulshakur <abdulshakur486@gmail.com>
2022-07-15 20:59:56 +0300

    edited.

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -2,7 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://audiophile-e-commerce.herokuapp.com/"
-      : "http://localhost:5010/",
+    process.env.NODE_ENV === "production" ? "/" : "http://localhost:5010/",
 });
