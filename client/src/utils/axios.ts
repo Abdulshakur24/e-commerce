e326ba5666642d55c9de363ee7556c7c863cf98f
abdulshakur <abdulshakur486@gmail.com>
2022-07-15 10:49:31 +0300

    initial commit.

---
+++ client/src/utils/axios.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://audiophile-e-commerce.herokuapp.com/"
+      : "http://localhost:5010/",
+});
