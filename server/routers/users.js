05a019133c854a4131cc612c10709e22bfca7362
abdulshakur <abdulshakur486@gmail.com>
2022-07-15 14:19:36 +0300

    added token middleware

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const userRouter = express.Router();
 
-const { register, login } = require("../controllers/controllers");
+const { register, login, token } = require("../controllers/controllers");
 
 userRouter.post("/register", register);
 
 userRouter.post("/login", login);
+
+userRouter.post("/token", token);
 
 module.exports = userRouter;
