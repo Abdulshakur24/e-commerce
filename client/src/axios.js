010b0f6ad47fcf9b1e64298a4149555213cb474d
abdulshakur <abdulshakur486@gmail.com>
2022-07-15 11:01:18 +0300

    migrated to typescript.

--- client/src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://audiophile-e-commerce.herokuapp.com/"
-      : "http://localhost:5010",
-  withCredentials: true,
-});
